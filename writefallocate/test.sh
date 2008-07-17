f1fcb2938e9fef90ffe587761944189841770352
Shehjar Tikoo <shehjart@gelato.unsw.edu.au>
2008-07-17 11:01:23 +1000

    Add simple shell script as a test harness.

---
+++ writefallocate/test.sh
@@ -0,0 +1,10 @@
+$FILESIZE=2147483648
+$WRBLKSIZE=65536
+$FALLOCBLKSIZE=1048576
+$STARTOFF=0
+$TESTFILE=/data/testfile
+$RUNS=1
+$FMODE=keepsize
+$MISCOPTIONS="--verbose"
+
+./writefallocate --filesize $FILESIZE --wrblksize $WRBLKSIZE --fallocate $FALLOCBLKSIZE --startoff $STARTOFF --filename $TESTFILE --runcount $RUNS --fallocmode=$FMODE $MISCOPTIONS 
